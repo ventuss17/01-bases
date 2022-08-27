863572f9b619454ef1dcb7429b382c6c00941495
Nicolas Venturini <ventuss17@gmail.com>
2022-08-26 22:32:38 -0300

    fix: angular 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+/*   styleUrls: ['./app.component.css'] */
 })
 export class AppComponent {
-  title = 'bases';
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
